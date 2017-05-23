88cd448fb59d44d961649bf321b52b35164f2b87
gaopin1534 <gaoxin.xixxix@gmail.com>
2017-05-23 17:38:14 +0900

    cosmetic

--- webViewTest/ViewController.swift
+++ webViewTest/ViewController.swift
@@ -19,36 +19,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var toolbar: UIToolbar!
     @IBOutlet weak var containerView: UIView!
     let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let webview = WKWebView()
         webview.navigationDelegate = self
         webview.translatesAutoresizingMaskIntoConstraints = false
-        
         containerView.addSubview(webview)
+        setUpConstraints(for: webview)
 
-        webview.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0.0).isActive = true
-        webview.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0.0).isActive = true
-        webview.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0.0).isActive = true
-        webview.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 0.0).isActive = true
-        
         let request = URLRequest(url: URL(string: "https://www.google.com")!)
         webview.load(request)
         
-        webview.rx.title.subscribe(onNext: {
-            print("title: \($0!)")
-        }).addDisposableTo(disposeBag)
-        
-        webview.rx.url.subscribe(onNext: {
-            print("title: \($0!)")
-        }).addDisposableTo(disposeBag)
+        webview.rx.title.bind(to: titleLabel.rx.text).addDisposableTo(disposeBag)
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func setUpConstraints(for webview: WKWebView) {
+        webview.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0.0).isActive = true
+        webview.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0.0).isActive = true
+        webview.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0.0).isActive = true
+        webview.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 0.0).isActive = true
     }
 
 }
