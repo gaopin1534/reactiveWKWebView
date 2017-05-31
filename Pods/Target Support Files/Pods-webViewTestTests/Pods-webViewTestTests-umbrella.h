0e8e7ebfda6c3fbef4fb9d02ab954e34208b31c4
高松幸平 <gaoxin.xixxix@gmail.com>
2017-05-31 22:20:27 +0900

    apply gitignore

--- Pods/Target Support Files/Pods-webViewTestTests/Pods-webViewTestTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_webViewTestTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_webViewTestTestsVersionString[];
-
--- Pods/Target Support Files/Pods-webViewTestUITests/Pods-webViewTestUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_webViewTestUITests : NSObject
-@end
-@implementation PodsDummy_Pods_webViewTestUITests
-@end
